e1779d03b108ed90a6e665a9d3c76299090d8336
Morgan Laco <morgan@codeschool.com>
2015-08-23 03:21:36 -0400

    Refactor reactor

--- reactor.rb
+++ reactor.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/ruby
 require 'pry'
 class Reactor
-  SPAWN_DIST_WIDTH = 5
-  SPAWN_DIST_OFFSET = 2
-  POINTS_FOR_DELTA = [1,13,15,3,2]
+  SPAWN_DIST_WIDTH = 7
+  SPAWN_DIST_OFFSET = 3
+  PRIORITIES = [4,1,0,2,3,5,6]
+  POINTS_FOR_PRIORITY = [90,73,70,33,30,15,3]
 
   def run
     warm_up
@@ -18,6 +19,7 @@ class Reactor
         probablize(potential_atom, delta)
       end
     end
+    fill_probability_holes
     normalize_probabilities
   end
 
@@ -31,7 +33,7 @@ class Reactor
         fluctuation -= p
       end
     end
-    # puts "initial_fluctuation: #{initial_fluctuation}\ntotal score: #{total_score}\nprobabilities: #{probabilities}"
+
     return 1
   end
 
@@ -48,24 +50,27 @@ class Reactor
   end
 
   def potential_atoms atom
-    # (0..SPAWN_DIST_WIDTH-1).map{|n| [[atom + n - SPAWN_DIST_OFFSET, 1].max, n] }
-    (0..SPAWN_DIST_WIDTH-1).map do |n|
-      val = atom + n - SPAWN_DIST_OFFSET
-      if val > 0
-        [val, n]
-      else
-        nil
-      end
-    end.compact
+    (0..SPAWN_DIST_WIDTH-1).map{|n| [[atom + n - SPAWN_DIST_OFFSET, 0].max, n] }.delete_if{|e,_| e<1}
   end
 
   def probablize atom, delta
     probability = probabilities[atom] || 0
-    probabilities[atom] = probability + POINTS_FOR_DELTA[delta]
+    probabilities[atom] = probability + points_for_delta(delta)
+  end
+
+  def points_for_delta delta
+    POINTS_FOR_PRIORITY[PRIORITIES[delta]]
   end
 
   def probabilities
     @probabilities ||= {}
+  end
+
+  def fill_probability_holes
+    extrema = probabilities.keys.minmax
+    (extrema[0]..extrema[1]).each do |mass|
+        probabilities[mass] = [1, (0.01 * total_score).round].max unless probabilities[mass]
+    end
   end
 
   def normalize_probabilities
@@ -85,11 +90,10 @@ class Reactor
   end
 end
 
-ring = [1]
+ring = [1,17]
 puts "initial ring: #{ring}"
 reactor = Reactor.new
-# (1..2).each do
-while ring.max < 15
+(1..20).each do
   reactor.ring = ring
   new_atom = reactor.run
   puts "#{new_atom} >> #{ring}\n\n"
